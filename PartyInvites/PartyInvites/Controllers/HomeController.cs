cb6d28d25f85e3c7b0ff5ba5d5fc2a90dce965c3
nzhul <dobromirivanov1@gmail.com>
2016-09-19 17:36:05 +0300

    PartyInvites init

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+			return View();
+		}
+
+		public ActionResult RsvpForm()
+		{
+			return View();
+		}
+	}
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PartyInvites.Models
+{
+	public class GuestResponse
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public bool? WillAttend { get; set; }
+	}
+}
