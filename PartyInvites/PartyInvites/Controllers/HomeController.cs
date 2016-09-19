d7a52b192adfabe2bacc7ad80f3f0d41d4d25ab0
nzhul <dobromirivanov1@gmail.com>
2016-09-19 22:19:12 +0300

    up-1019

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -25,8 +25,16 @@ namespace PartyInvites.Controllers
 		[HttpPost]
 		public ViewResult RsvpForm(GuestResponse guestResponse)
 		{
-			//TODO: Email response to the party organizer
-			return View("Thanks", guestResponse);
+			if (ModelState.IsValid)
+			{
+				// TODO: Email response to the party organizer
+				return View("Thanks", guestResponse);
+			}
+			else
+			{
+				// there is a validation error
+				return View();
+			}
 		}
 	}
 }
