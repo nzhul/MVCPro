b22a1266738480f81b2e4cf9739fa5f87ac21426
nzhul <dobromirivanov1@gmail.com>
2016-10-10 01:06:21 +0300

    update 10-10-2016 01:06

--- SportsStore/SportsStore.Domain/Entities/Product.cs
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -7,11 +7,19 @@ namespace SportsStore.Domain.Entities
 	{
 		[HiddenInput(DisplayValue = false)]
 		public int ProductID { get; set; }
+
+		[Required(ErrorMessage = "Please enter product name")]
 		public string Name { get; set; }
 
 		[DataType(DataType.MultilineText)]
+		[Required(ErrorMessage = "Please enter a description")]
 		public string Description { get; set; }
+
+		[Required]
+		[Range(0.01, double.MaxValue, ErrorMessage = "Please enter a possitive price")]
 		public decimal Price { get; set; }
+
+		[Required(ErrorMessage = "Please specify a category")]
 		public string Category { get; set; }
 	}
 }
