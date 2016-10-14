a47dd4603b43c75ac3849ae82930072e8fd7c1a5
nzhul <dobromirivanov1@gmail.com>
2016-10-14 22:23:41 +0300

    update

---
+++ SportsStore/SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.UnitTests
+{
+	[TestClass]
+	public class AdminSecurityTests
+	{
+		[TestMethod]
+		public void CanLogin_WithValidCredentials()
+		{
+			// arrange - create a mock authentication provider
+			Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+			mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+
+			// arrange - create the view model
+			LoginViewModel model = new LoginViewModel
+			{
+				UserName = "admin",
+				Password = "secret"
+			};
+
+			// arrange - create the controller
+		}
+	}
+}
