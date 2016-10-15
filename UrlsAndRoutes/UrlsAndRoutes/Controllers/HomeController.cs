f10662772898c05b8e77e4f2101d26b78a42dde6
Dobromir Ivanov <dobromirivanov1@gmail.com>
2016-10-15 21:17:30 +0300

    update 15-10-2016 21:17

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Controller = "Home";
+			ViewBag.Action = "Index";
+			return View("ActionName");
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
