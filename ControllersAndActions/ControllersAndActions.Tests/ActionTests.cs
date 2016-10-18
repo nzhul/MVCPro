714c7cce0906cceea1af80f97b92dd3b2cf95db1
nzhul <dobromirivanov1@gmail.com>
2016-10-19 02:17:32 +0300

    update 19-10-2016 02:17

---
+++ ControllersAndActions/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,23 @@
+﻿using ControllersAndActions.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Tests
+{
+	[TestClass]
+	public class ActionTests
+	{
+		[TestMethod]
+		public void ViewSelectionTest()
+		{
+			// arrange - create the controller
+			ExampleController target = new ExampleController();
+
+			// act - call the action method
+			ViewResult result = target.Index();
+
+			// assert - check the result
+			Assert.AreEqual("HomePage", result.ViewName);
+		}
+	}
+}
